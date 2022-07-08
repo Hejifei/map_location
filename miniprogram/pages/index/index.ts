817f769d3d5cf27b8c2ccb1958db8b9062e7ab3c
HeJifei <jifei.he@goodwe.com>
2022-07-08 10:47:01 +0800

    添加地图移动、自动在页面中间添加mark功能

--- miniprogram/pages/index/index.ts
+++ miniprogram/pages/index/index.ts
@@ -2,6 +2,9 @@
 // 获取应用实例
 const app = getApp<IAppOption>()
 
+
+export const CDN_PATH = 'https://3gimg.qq.com/lightmap/xcx/demoCenter/images';
+
 Page({
   data: {
     motto: 'Hellow 小程序',
@@ -12,11 +15,22 @@ Page({
     canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName'), // 如需尝试获取用户信息可改为false
     location: {
         latitude: 40.040415,
-        longitude: 116.273511
+        longitude: 116.273511,
     },
     showPosition: true,
     isLocationPersionAllowed: false,
     isLogin: false,
+    isRegionChanged: false, //  视野是否变换,若变化了,显示mark并设置中心点坐标为标注点
+    markers: [
+      {
+        id: 0,
+        iconPath: `${CDN_PATH}/Marker3_Activated@3x.png`,
+        latitude: 40.040415,
+        longitude: 116.273511,
+        width: 30,
+        height: 30
+      }
+    ],
   },
   // 事件处理函数
   bindViewTap() {
@@ -78,5 +92,55 @@ Page({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
     })
-  }
+  },
+  // 监听视野变化
+	onChangeRegion (event: any) {
+		if (event.type === 'end' && event.causedBy === 'drag') {
+			const mapCtx = wx.createMapContext('map', this);
+			mapCtx.getCenterLocation({
+				success: res => {
+					const latitude = res.latitude;
+					const longitude = res.longitude;
+					this.setData({
+						// regionCallbackTxt: '中心点坐标：' + latitude.toFixed(6) + ',' + longitude.toFixed(6),
+            location: {
+              latitude: latitude,
+              longitude: longitude,
+            },
+            isRegionChanged: true,
+            markers: [{
+              id: 0,
+              iconPath: `${CDN_PATH}/Marker3_Activated@3x.png`,
+              latitude: latitude,
+              longitude: longitude,
+              width: 30,
+              height: 30
+            }]
+					});
+				}
+			});
+		}
+	},
+  // poi点击回调
+	onTapPoi (event: any) {
+		// const name = event.detail.name.length <= 8 ? event.detail.name : event.detail.name.substring(0, 8)+'...';
+		const latitude = event.detail.latitude;
+		const longitude = event.detail.longitude;
+		this.setData({
+			// poiCallbackTxt: name + '：' + latitude.toFixed(6) + ',' + longitude.toFixed(6)
+      location: {
+        latitude: latitude,
+        longitude: longitude,
+      },
+      isRegionChanged: true,
+      markers: [{
+        id: 0,
+        iconPath: `${CDN_PATH}/Marker3_Activated@3x.png`,
+        latitude: latitude,
+        longitude: longitude,
+        width: 30,
+        height: 30
+      }]
+		});
+	},
 })
